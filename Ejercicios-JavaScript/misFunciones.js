306d2cde93bb72946d69d82c631025215a903131
joaokxt <joaokx.terck@gmail.com>
2023-05-17 08:38:16 -0300

    Ejercicio Conversor de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,3 +6,34 @@
  * @return Valor que retorna
  */
 
+function conversorUnidades(id, valor){
+    if(isNaN(valor)){
+        document.lasUnidades.unid_yarda.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_metro.value = "";
+        alert("Se ingresó un valor inválido en " + id);
+    }
+    else{
+        if(id=="metro"){
+            document.lasUnidades.unid_yarda.value = valor*1.094;
+            document.lasUnidades.unid_pie.value = valor*3.28084;
+            document.lasUnidades.unid_pulgada.value = valor*39.3701;
+        }
+        if(id=="yarda"){
+            document.lasUnidades.unid_metro.value = valor/1.094;
+            document.lasUnidades.unid_pie.value = valor*3;
+            document.lasUnidades.unid_pulgada.value = valor*36;
+        }
+        if(id=="pie"){
+            document.lasUnidades.unid_metro.value = valor/3.28084;
+            document.lasUnidades.unid_yarda.value = valor/3;
+            document.lasUnidades.unid_pulgada.value = valor*12;
+        }
+        if(id=="pulgada"){
+            document.lasUnidades.unid_metro.value = valor/39.3701;
+            document.lasUnidades.unid_yarda.value = valor/36;
+            document.lasUnidades.unid_pie.value = valor/12;
+        }
+    }
+}
